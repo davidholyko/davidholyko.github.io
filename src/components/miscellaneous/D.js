bd1f589069c26a4999335c04627c1502810b7953
David Ko <david.holy.ko@gmail.com>
2020-07-11 00:05:44 -0400

    wip use single D

---
+++ src/components/miscellaneous/D.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+/**
+ * references:
+ *   https://css-tricks.com/svg-path-syntax-illustrated-guide/
+ *   https://css-tricks.com/svg-line-animation-works/
+ *   https://css-tricks.com/guide-svg-animations-smil/
+ */
+
+class D extends React.Component {
+  constructor(props) {
+    super(props);
+    this.pathRef = React.createRef();
+
+    this.state = {
+      sharedStrokeWidth: '10px',
+    };
+
+    window.Name = this;
+  }
+
+  render() {
+    return (
+      <div className="full-viewport">
+        <svg
+          className="svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 50 50"
+          width="500px"
+          height="500px"
+        >
+          <path
+            className="path"
+            id="D"
+            fill="none"
+            stroke="#ffffff"
+            strokeWidth="1px"
+            d={`
+            M 7.5, 46
+            v -42.5
+            h 15
+            c 2.88, 0
+              5.509, 0.47
+              7.89, 1.41
+            c 2.38, 0.94
+              4.425, 2.31
+              6.135, 4.11
+            s 3.04, 4
+              3.99, 6.6
+            c 0.95, 2.6
+              1.425, 5.56
+              1.425, 8.88
+            c 0, 3.32 
+              -0.476, 6.28 
+              -1.425, 8.88
+            c -0.95, 2.6
+              -2.28, 4.8
+              -3.99, 6.6
+            s -3.755, 3.17
+              -6.135, 4.11
+            c -2.38, 0.94
+              -5.01, 1.41
+              -7.89, 1.41
+            h-15`}
+          />
+        </svg>
+      </div>
+    );
+  }
+}
+
+export default D;
--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import Loading from './components/miscellaneous/Loading';
-import Name from './components/miscellaneous/Name';
+import D from './components/miscellaneous/D';
 
 const App = React.lazy(() => import('./components/App'));
 
 let appJsx = (
   <BrowserRouter>
     {/* <Suspense fallback={Loading}> */}
-    <Suspense fallback={Name}>
+    <Suspense fallback={D}>
       <App />
     </Suspense>
   </BrowserRouter>
 );
 
 appJsx = <Loading />;
-appJsx = <Name />;
+appJsx = <D />;
 
 ReactDOM.render(appJsx, document.getElementById('root'));
