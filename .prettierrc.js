5d9ceb2954c0cf84d0ff3c09839995c5f0ef2394
davidholyko <david.holy.ko@gmail.com>
2025-05-30 21:21:21 -0400

    feat(app): use vitest latest ts react template

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'es5',
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+# Portfolio Site
+
+## Install
+
+```
+nvm install
+nvm use
+npm ci
+```
+
+## Development
+
+```
+npm run dev
+```
+
+## Deployment
+
+```
+npm run build
+```
